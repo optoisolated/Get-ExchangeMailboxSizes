272edac8dd352d7285fcac80585ba2ba6d7bb283
Optoisolated <46586989+optoisolated@users.noreply.github.com>
2019-05-29 15:52:31 +1000

    Fixed bugs with CSV Import Handling

--- Get-ExchangeMailboxSizes.ps1
+++ Get-ExchangeMailboxSizes.ps1
@@ -3,12 +3,15 @@ Param(
     [string]$OutCSV = $(throw "-OutCSV is required.")
 )
 
+CLS
+
 $Users = Import-CSV $UserCSV 
 $SizingDump = @()
 
 ForEach ($User in $Users) {
-    $UserSizing = Get-Mailbox -Identity $($User.User) | Get-MailboxStatistics | `
-    Select-Object DisplayName, @{name=”TotalItemSize (GB)”;expression={[math]::Round((($_.TotalItemSize.Value.ToString()).Split(“(“)[1].Split(” “)[0].Replace(“,”,””)/1GB),2)}},ItemCount
+    $UserName = $User.User
+    "Checking $Username"
+    $UserSizing = Get-Mailbox -Identity $Username | Get-MailboxStatistics | Select-Object DisplayName, @{name=”TotalItemSize (GB)”;expression={[math]::Round((($_.TotalItemSize.Value.ToString()).Split(“(“)[1].Split(” “)[0].Replace(“,”,””)/1GB),2)}},ItemCount
     $SizingDump += $UserSizing
 }
 
