99d7114ec5e563e4fa558e91cfa1f36f2e7ce715
Optoisolated <46586989+optoisolated@users.noreply.github.com>
2019-05-29 15:20:53 +1000

    Made the script parametered. (with user input)

--- Get-ExchangeMailboxSizes.ps1
+++ Get-ExchangeMailboxSizes.ps1
@@ -1,9 +1,11 @@
-$UserList = "C:\temp\mailboxes.csv"
-$OutputFile = "C:\temp\MailboxSizes.csv"
+Param(
+    [string]$UserCSV = $(throw "-UserCSV is required."),
+    [string]$OutCSV = $(throw "-OutCSV is required.")
+)
 
-Import-CSV $UserList | `
+Import-CSV $UserCSV | `
 Get-Mailbox -Identity $_.User | `
 Get-MailboxStatistics | `
 Select-Object DisplayName, @{name=”TotalItemSize (GB)”;expression={[math]::Round((($_.TotalItemSize.Value.ToString()).Split(“(“)[1].Split(” “)[0].Replace(“,”,””)/1GB),2)}},ItemCount | `
 Sort “TotalItemSize (GB)” -Descending | `
-Export-CSV $OutputFile
+Export-CSV $OutCSV
